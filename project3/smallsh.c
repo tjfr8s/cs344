e4677b576d294a7a24c2b7bedff256f9dbda7a9a
tyler <tjfr8s@gmail.com>
2019-05-15 20:09:58 -0700

    execute forground processes and builtins.

--- project3/smallsh.c
+++ project3/smallsh.c
@@ -1,10 +1,15 @@
 #define _POSIX_C_SOURCE 1
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <errno.h>
+#include <dirent.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "smallsh.h"
 // Prompt user for input and recieve this input.
 
@@ -39,37 +44,183 @@
 // - open input files as wonly and output as ronly 
 // - error and status 1 on fail
 int main(int argc, char** argv) {
-    size_t  maxCommandStringLength = MAX_COMMAND_LENGTH + 1;
-    char    commandString[maxCommandStringLength];
-    char*   argArray[MAX_ARGS] = {};
-    int     numArgs = 0;
+    size_t              maxCommandStringLength = MAX_COMMAND_LENGTH + 1;
+    char                commandString[maxCommandStringLength];
+    char*               argArray[MAX_ARGS + 1] = {};
+    int                 numArgs = 0;
+    int                 prevExitStatus = 0;
+    struct sigaction    SIGINT_action = {{0}};
+    SIGINT_action.sa_handler = shellHandleSIGINT;
+    SIGINT_action.sa_flags = SA_RESTART;
+    sigfillset(&SIGINT_action.sa_mask);
+    sigaction(SIGINT, &SIGINT_action, NULL);
 
-    memset(&commandString, 0, maxCommandStringLength);
-    getInputString(commandString);
-    printf("The input string: %s\n", commandString);
+    while(1) {
+        memset(&commandString, 0, maxCommandStringLength);
+        getInputString(commandString);
+        printf("The input string: %s\n", commandString);
+        fflush(stdout);
 
-    tokenizeArguments(commandString, MAX_ARGS, &numArgs, argArray);
-    freeArgArray(argArray, numArgs);
+        parseArguments(commandString, MAX_ARGS, &numArgs, argArray, &prevExitStatus);
+        freeArgArray(argArray, &numArgs);
+    }
 
     return 0;
 }
 
+void lsBuiltIn() {
+    struct dirent*  entry;
+    DIR*            dr = opendir(".");
 
+    if (dr == NULL) {
+        perror("couldn't open dir");
+        return;
+    }
+
+    while ((entry = readdir(dr)) != NULL) {
+        printf("%s ", entry->d_name);
+        fflush(stdout);
+    }
+
+    closedir(dr);
+    printf("\n");
+    fflush(stdout);
+    return;
+}
+
+bool isBuiltIn(char* command) {
+    char*   builtIns[4] = {"exit", "ls", "cd", "status"};
+    int     i;
+    for(i = 0; i < 4; i++) {
+        if (!strcmp(command, builtIns[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool hasValidNumArgs(char** argArray, 
+                     int numArgs, 
+                     int maxArgs, 
+                     int minArgs, 
+                     bool isBuiltIn) {
+    if (numArgs < minArgs) {
+        perror("too few arguments\n");
+        return false;
+    } else if ((numArgs == maxArgs + 1) 
+            && isBuiltIn 
+            && !strcmp(argArray[numArgs - 1], "&")) {
+        return true; 
+    } else if (numArgs > maxArgs) {
+        perror("too many arguments\n");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void executeBuiltIn(char** argArray, int numArgs, int prevExitStatus) {
+    if (!strcmp("ls", argArray[0]) 
+            && hasValidNumArgs(argArray, numArgs, 1, 1, true)) {
+        lsBuiltIn();
+                 
+    } else if (!strcmp("exit", argArray[0])
+            && hasValidNumArgs(argArray, numArgs, 1, 1, true)) {
+        freeArgArray(argArray, &numArgs);
+        exit(0);
+    } else if (!strcmp("cd", argArray[0])
+           && hasValidNumArgs(argArray, numArgs, 2, 1, true)) {
+        char* path = NULL;
+        if (numArgs == 1) {
+            chdir(getenv("HOME"));
+        } else {
+            path = argArray[1];
+            chdir(path);
+        }
+        
+    } else if (!strcmp("status", argArray[0])
+           && hasValidNumArgs(argArray, numArgs, 1, 1, true)) {
+        printf("status: %d\n", prevExitStatus);
+    } 
+}
+
+void executeCommand(char** argArray, int* numArgs, int* prevExitStatus) {
+    int i;
+    for (i = 0; i < *numArgs; i++) {
+        printf("arg%d: %s\n", i, argArray[i]);
+    }
+    fflush(stdout);
+    pid_t               pid;
+    int                 status = -5;
+    struct sigaction    SIGINT_action_child = {{0}};
+    bool                isBackground = false;
+
+    SIGINT_action_child.sa_handler = childHandleSIGINT;
+    SIGINT_action_child.sa_flags = SA_RESTART;
+    sigfillset(&SIGINT_action_child.sa_mask);
+
+    if (!strcmp(argArray[*numArgs - 1], "&")) {
+        free(argArray[*numArgs - 1]);
+        argArray[*numArgs - 1] = NULL;
+        (*numArgs)--;
+        isBackground = true;
+    }
+
+    pid = fork();
+    switch(pid) {
+        case -1:
+            perror("\nerror creating new process\n");
+        case 0:
+            // Child process, execute command
+            // Set appropraite handler funciton for signals.
+            sigaction(SIGINT, &SIGINT_action_child, NULL);
+            if (execvp(argArray[0], argArray) > 0) {
+                exit(1);
+            }
+        default:
+            printf("waiting\n");
+            fflush(stdout);
+            if (waitpid(pid, &status, 0) == -1) {
+                printf("\nwaitpid error %s\n", strerror(errno));
+            }
+            printf("status: %d\n", status);
+            if (WIFEXITED(status) != 0) {
+                printf("finished waiting | exit status: %d\n", WEXITSTATUS(status));
+                *prevExitStatus = WEXITSTATUS(status);
+            } else {
+                printf("finished waiting | signal value: %d\n", WTERMSIG(status));
+                *prevExitStatus = WTERMSIG(status);
+            }
+            fflush(stdout);
+    }
+}
 
 int parseArguments(char* commandString, 
                   int maxArgs, 
                   int* numArgs, 
-                  char** argArray) {
-    tokenizeArguments(commandString, maxArgs, numArgs, argArray);
+                  char** argArray,
+                  int* prevExitStatus) {
+    if (tokenizeArguments(commandString, maxArgs, numArgs, argArray) == -1) {
+        perror("error tokenizing argument string\n");
+        return -1;
+    }
 
+    if (isBuiltIn(argArray[0])) {
+        executeBuiltIn(argArray, *numArgs, *prevExitStatus);
+    } else  {
+        executeCommand(argArray, numArgs, prevExitStatus);
+    
+    }
     return 0;
 }
 
-void freeArgArray(char** argArray, int numArgs) {
+void freeArgArray(char** argArray, int* numArgs) {
     int curArg = 0;
-    for(curArg = 0; curArg < numArgs; curArg++) {
+    for(curArg = 0; curArg < *numArgs; curArg++) {
         free(argArray[curArg]);
+        argArray[curArg] = NULL;
     }
+    *numArgs = 0;
 }
 
 /*******************************************************************************
@@ -96,7 +247,6 @@ int tokenizeArguments(char* commandString,
             perror("Too many arguments");
             return -1;
         }
-        printf("token: %s\n", token);
 
         if((argArray[*numArgs] = 
                     malloc(sizeof(char) * (strlen(token) + 1))) == NULL) {
@@ -112,9 +262,15 @@ int tokenizeArguments(char* commandString,
     return 0;
 }
 
-void getlineHandleSIGINT(int signum) {
-    char* message = "SIGINT received\n";
+void shellHandleSIGINT(int signum) {
+    char* message = "\nSIGINT received\n";
     write(STDOUT_FILENO, message, strlen(message) + 1);
+}
+
+void childHandleSIGINT(int signum) {
+    char* message = "\nChild SIGINT received\n";
+    write(STDOUT_FILENO, message, strlen(message) + 1);
+    _exit(signum);
 }
 
 /*******************************************************************************
@@ -125,10 +281,6 @@ void getlineHandleSIGINT(int signum) {
  *                                  command.
 *******************************************************************************/
 void getInputString(char* commandString) {
-    struct sigaction   SIGINT_action = {{0}};
-    SIGINT_action.sa_handler = getlineHandleSIGINT;
-    sigfillset(&SIGINT_action.sa_mask);
-    sigaction(SIGINT, &SIGINT_action, NULL);
 
     size_t      bufferSize = 0;
     char*       inputBuffer = NULL;
@@ -137,11 +289,11 @@ void getInputString(char* commandString) {
 
     while(1) {
         printf("Enter a command: ");
+        fflush(stdout);
         numRead = getline(&inputBuffer, &bufferSize, stdin);
         if (numRead == -1) {
             clearerr(stdin);
         } else if (numRead > MAX_COMMAND_LENGTH + 1) {
-            printf("Input exceeded %d characters!\n", MAX_COMMAND_LENGTH);
             free(inputBuffer);
             inputBuffer = NULL;
         } else {
@@ -149,9 +301,7 @@ void getInputString(char* commandString) {
         }
     }
 
-
     inputBuffer[strcspn(inputBuffer, "\n")] = '\0';
-    printf("This is what you entered: \"%s\"\n", inputBuffer);
     strcpy(commandString, inputBuffer);
     free(inputBuffer);
     inputBuffer = NULL;
--- project3/smallsh.h
+++ project3/smallsh.h
@@ -4,10 +4,16 @@
 #define MAX_COMMAND_LENGTH 2048
 #define MAX_ARGS 512
 
-int     parseArguments(char* commandString, int maxArgs, int* numArgs, char** argArray);
-void    freeArgArray(char** argArray, int numArgs);
+void    lsBuiltIn();
+bool    isBuiltIn(char* command);
+bool    hasValidNumArgs(char** argArray, int numArgs, int maxArgs, int minArgs, bool isBuiltIn);
+void    executeBuiltIn (char** argArray, int numArgs, int prevExitStatus);
+void    executeCommand(char** argArray, int * numArgs, int* prevExitStatus);
+int     parseArguments(char* commandString, int maxArgs, int* numArgs, char** argArray, int* prevExitStatus);
+void    freeArgArray(char** argArray, int* numArgs);
 int     tokenizeArguments(char* commandString, int maxArgs, int* numArgs, char** argArray);
-void    getlineHandleSIGINT(int signum);
+void    shellHandleSIGINT(int signum);
+void    childHandleSIGINT(int signum);
 void    getInputString(char* stringBuffer);
 
 #endif 
