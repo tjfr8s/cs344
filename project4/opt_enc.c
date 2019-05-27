d7cc3bf364ef364211d170513050125003a534c5
tyler <tjfr8s@gmail.com>
2019-05-27 16:47:03 -0700

    send both files from enc to enc_d

---
+++ project4/opt_enc.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
+
+void send_to_server(int sockfd, char* filename) {
+    char buffer[1024];
+    FILE* fp;
+    int charsWritten;
+    int charsRead;
+    int numRead;
+	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
+    fp = fopen(filename, "rb");
+
+    while ((numRead = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+        charsWritten = send(sockfd, buffer, strlen(buffer), 0); // Write to the server
+        if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+        if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+        // Get return message from server
+        memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+        charsRead = recv(sockfd, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
+        if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+        printf(buffer);
+        memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+	int socketFD, portNumber;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+    
+	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+
+	// Set up the server address struct
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+	// Set up the socket
+	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (socketFD < 0) error("CLIENT: ERROR opening socket");
+	
+	// Connect to server
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+		error("CLIENT: ERROR connecting");
+
+    send_to_server(socketFD, "./plaintext4");
+    send_to_server(socketFD, "./keyfile");
+
+	close(socketFD); // Close the socket
+	return 0;
+}
---
+++ project4/opt_enc_d.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+void receive_file(int sockfd, FILE* tempfd) {
+	char buffer[BUFFER_SIZE];
+    int charsRead;
+    int end;
+	memset(buffer, '\0', BUFFER_SIZE);
+
+    while ( strstr(buffer, "\n") == NULL) {
+        // Get the message from the client and display it
+        memset(buffer, '\0', BUFFER_SIZE);
+        charsRead = recv(sockfd, buffer, BUFFER_SIZE - 1, 0); // Read the client's message from the socket
+        if (charsRead < 0) error("ERROR reading from socket");
+        fwrite(buffer, BUFFER_SIZE, 1, tempfd);
+        // Send a Success message back to the client
+        charsRead = send(sockfd, "ok", 3, 0); // Send success back
+        if (charsRead < 0) error("ERROR writing to socket");
+    }
+}
+
+void read_file(FILE* tempfd) {
+    char buffer[1024];
+    int numRead;
+	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
+
+    rewind(tempfd);
+
+    while ((numRead = fread(buffer, 1, sizeof(buffer), tempfd)) > 0) {
+        printf(buffer);
+        memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
+    }
+    
+}
+
+int main(int argc, char *argv[])
+{
+	int listenSocketFD, establishedConnectionFD, portNumber;
+    FILE* textfile;
+    FILE* keyfile;
+	socklen_t sizeOfClientInfo;
+	struct sockaddr_in serverAddress, clientAddress;
+
+	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+
+	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+	// Set up the socket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	// Enable the socket to begin listening
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+		error("ERROR on binding");
+	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+
+	// Accept a connection, blocking if one is not available until one connects
+	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+
+	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+	if (establishedConnectionFD < 0) error("ERROR on accept");
+    textfile = tmpfile();
+    receive_file(establishedConnectionFD, textfile);
+    read_file(textfile);
+    printf("\n\n\n\n\n\n");
+    keyfile = tmpfile();
+    receive_file(establishedConnectionFD, keyfile);
+    read_file(keyfile);
+
+	close(establishedConnectionFD); // Close the existing socket which is connected to the client
+
+	close(listenSocketFD); // Close the listening socket
+	return 0; 
+}
